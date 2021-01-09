80b4a1d6b3e023c5dc9f08e27ac0a5ca91570f1a
orlandobel <orlando.bel.jimmy@gmail.com>
2021-01-09 16:37:38 -0600

    vista de login

--- SistemaWeb/resources/views/auth/login.blade.php
+++ SistemaWeb/resources/views/auth/login.blade.php
@@ -7,25 +7,25 @@ registro
 @section('content')
 <div class="login-box">
     <div class="login-logo">
-        <a href="#"> <b>Admin</b>LTE </a>
+        <a href="#"> <b>NotiPush</b> </a>
     </div>
 
     <div class="card">
         <div class="card-body login-card-body">
-            <p class="login-box-msg">Sign in to start your session</p>
+            <p class="login-box-msg">Inicio de sesión</p>
 
             <form action="#" method="post">
                 <div class="input-group mb-3">
-                    <input type="email" placeholder="Email" class="form-control">
+                    <input type="text" placeholder="Numero de boleta o de empleado" class="form-control">
                     <div class="input-group-append">
                         <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
+                            <span class="fas fa-user"></span>
                         </div>
                     </div>
                 </div>
 
                 <div class="input-group mb-3">
-                    <input type="password" class="form-control" placeholder="Password">
+                    <input type="password" class="form-control" placeholder="Contraseña">
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-lock"></span>
@@ -33,40 +33,23 @@ registro
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="col-8">
+                <div class="row ">
+                    <!--div class="col-8">
                         <div class="icheck-primary">
                             <input type="checkbox" id="remember">
                             <label for="remember">
                                 Remember Me
                             </label>
                         </div>
-                    </div>
+                    </div-->
                     <!-- /.col -->
                     <div class="col-4">
-                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                        <!--button type="submit" class="btn btn-primary btn-block">Entrar</button-->
+                        <a href="{{ route('home') }}" class="btn btn-primary">Entrar</a>
                     </div>
                     <!-- /.col -->
                 </div>
             </form>
-
-            <div class="social-auth-links text-center mb-3">
-                <p>- OR -</p>
-                <a href="#" class="btn btn-block btn-primary">
-                    <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-                </a>
-                <a href="#" class="btn btn-block btn-danger">
-                    <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-                </a>
-            </div>
-            <!-- /.social-auth-links -->
-
-            <p class="mb-1">
-                <a href="forgot-password.html">I forgot my password</a>
-            </p>
-            <p class="mb-0">
-                <a href="register.html" class="text-center">Register a new membership</a>
-            </p>
         </div>
     </div>
 </div>
