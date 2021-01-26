6028456ad51ae28b3d568cc09f150aaae9584d3b
orlandobel <orlando.bel.jimmy@gmail.com>
2021-01-26 17:51:07 -0600

    apk de la app

--- SistemaWeb/resources/views/layouts/app.blade.php
+++ SistemaWeb/resources/views/layouts/app.blade.php
@@ -173,11 +173,8 @@
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{asset('Templates/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
-        </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{Auth::user()->nombre}}</a>
         </div>
       </div>
 
